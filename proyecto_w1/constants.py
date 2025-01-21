5860c289d8efe239439d442657d2cf80dc85d543
Juan <juan.technova@gmail.com>
2025-01-21 20:26:17 +0100

    feat: created the simple elements for the stack

---
+++ proyecto_w1/constants.py
@@ -0,0 +1,30 @@
+ENTRY_LAMBDA_CODE="""
+import boto3, uuid
+
+client = boto3.resource('dynamodb')
+table = client.Table("orders")
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        print(str(payload))
+        table.put_item(Item= {'orderID': str(uuid.uuid4()),'order':  payload})
+"""
+NOTIFY_LAMBDA_CODE="""
+import boto3, json
+
+client = boto3.client('sns')
+
+def lambda_handler(event, context):
+
+    for record in event["Records"]:
+
+        if record['eventName'] == 'INSERT':
+            new_record = record['dynamodb']['NewImage']    
+            response = client.publish(
+                TargetArn='<Enter Amazon SNS ARN for the POC-Topic>',
+                Message=json.dumps({'default': json.dumps(new_record)}),
+                MessageStructure='json'
+            )
+"""
--- proyecto_w1/proyecto_w1_stack.py
+++ proyecto_w1/proyecto_w1_stack.py
@@ -1,19 +1,53 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as db,
+    aws_apigateway as api,
+    aws_sqs as sqs,
+    aws_lambda as lambda_func,
+    aws_sns as sns,
+    RemovalPolicy,
+    Duration
 )
 from constructs import Construct
+from . import constants
 
 class ProyectoW1Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        database = db.Table(
+            self, "order_table",
+            table_name="OrderTable",
+            partition_key=db.Attribute(
+                name="OrderId",
+                type=db.AttributeType.STRING
+            ),
+            billing_mode=db.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ProyectoW1Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        entry_func = lambda_func.Function(scope=scope,id="lambda_entry",
+                             runtime=lambda_func.Runtime.PYTHON_3_10,
+                             function_name="lambda_entry",
+                             code=lambda_func.Code.from_inline(constants.ENTRY_LAMBDA_CODE)
+        )
+
+        notify_func = lambda_func.Function(scope=scope,id="lambda_notify",
+                             runtime=lambda_func.Runtime.PYTHON_3_10,
+                             function_name="lambda_notify",
+                             code=lambda_func.Code.from_inline(constants.ENTRY_LAMBDA_CODE)
+        )
+
+        queue = sqs.Queue(
+            scope, "MyQueue",
+            queue_name="MyQueue",
+            fifo=False
+        )
+
+        topic = sns.Topic(
+            self, "MyTopic",
+            topic_name="MyTopic",
+            display_name="My SNS Topic for Notifications"
+        )
